fa54c0122ac20000ed8e735535fbebd1f2c0f3bc
pm066062 <partha.mondal@cerner.com>
2019-07-23 19:31:39 +0530

    first commit

---
+++ src/main/java/com/partha/JavaCustomAnnotation/JavaCustomAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.partha.JavaCustomAnnotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCustomAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCustomAnnotationApplication.class, args);
+	}
+
+}
